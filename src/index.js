4bcf0f1cdb88464fda528041e36e5b1e24ccc59f
jjjpanda <jawpanda13@gmail.com>
2019-09-28 22:48:04 -0400

    updates

--- src/index.js
+++ src/index.js
@@ -9,21 +9,34 @@ import {
   Button,
 } from 'antd';
 
-import "./css/logo.css"
+import "./css/logo.css";
 
-const onChange = e => {
-  console.log(e);
-};
+class StockSymbol extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {data:0};
+  }
+
+  onChange = e => {
+    e.persist()
+    //console.log(e);
+    this.setState({ data: e.target.value });
+  };
+  
+  render() { return (<div className="App">
+      <div>
+        <Input placeholder="input with clear icon" allowClear onChange={this.onChange} />
+        <pre>{this.state.data}</pre>
+      </div>
+    </div>);
+  }
+}
 
 ReactDOM.render(
   [
     <img id = "logo" className = "spin" src={logo}></img>,
     <h1>Outsmart Options</h1>,
-    <div className="App">
-      <div>
-        <Input placeholder="input with clear icon" allowClear onChange={onChange} />
-      </div>
-    </div>
+    <StockSymbol />
   ],
   document.getElementById('root')
 );
