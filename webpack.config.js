4dad8433f8266c8c7e29f691e0e42788213dcf63
jjjpanda <jawpanda13@gmail.com>
2019-10-01 21:30:51 -0400

    package and webpack config updates for server

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  watch: true,
   mode: 'development',
   entry: './src/index.js',
   output: {
