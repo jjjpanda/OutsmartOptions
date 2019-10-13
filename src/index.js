add67238683f14ec3ef3bc7e71f358da46b6f6e2
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2019-10-13 19:01:30 -0400

    recharts mock graph

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ import {
 } from 'antd';
 const { Search } = Input
 const { Panel } = Collapse
+import {XAxis, YAxis, CartesianGrid, Tooltip, LineChart, Line} from 'recharts';
 
 //JS Libraries
 import * as optionsMath from './jsLib/optionsMathLibrary.js'
@@ -32,6 +33,61 @@ import "./css/calculator.less";
 post.fetchReq('/treasury', '', (data) => {
   console.log(data)
 })
+
+const mockGraphData = [
+  {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
+  {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
+  {name: 'Page C', uv: -1000, pv: 9800, amt: 2290},
+  {name: 'Page D', uv: 500, pv: 3908, amt: 2000},
+  {name: 'Page E', uv: -2000, pv: 4800, amt: 2181},
+  {name: 'Page F', uv: -250, pv: 3800, amt: 2500},
+  {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+];
+
+const gradientOffset = () => {
+  const dataMax = Math.max(...mockGraphData.map((i) => i.uv));
+  const dataMin = Math.min(...mockGraphData.map((i) => i.uv));
+
+  if (dataMax <= 0){
+  	return 0
+  }
+  else if (dataMin >= 0){
+  	return 1
+  }
+  else{
+  	return dataMax / (dataMax - dataMin);
+  }
+}
+
+const off = gradientOffset();
+
+class SimpleAreaChart extends React.Component{
+  constructor(props){
+    super(props)
+  }
+	render () {
+  	return (
+    	<LineChart
+        width={600}
+        height={400}
+        data={mockGraphData}
+        margin={{top: 10, right: 30, left: 0, bottom: 0}}
+      >
+        <CartesianGrid strokeDasharray="3 3"/>
+        <XAxis dataKey="name"/>
+        <YAxis/>
+        <defs>
+          <linearGradient id="splitColor" x1="0" y1="0" x2="0" y2="1">
+            <stop offset={off} stopColor="green" stopOpacity={1}/>
+            <stop offset={off} stopColor="red" stopOpacity={1}/>
+          </linearGradient>
+        </defs>
+        <Tooltip/>
+        <Line type="monotone" dataKey="uv" stroke="#000" stroke="url(#splitColor)" />
+      </LineChart>
+    );
+  }
+}
 
 class OptionsCalculator extends React.Component{
   constructor(props){
@@ -433,7 +489,8 @@ ReactDOM.render(
   [
     <img key="mainLogo" id = "logo" className = "spin" src={logo}></img>,
     <h1 key = "mainTitle" style={{paddingLeft:'60px'}}>Outsmart Options</h1>,
-    <OptionsCalculator key="theVoiceOfThePeople"/>
+    <OptionsCalculator key="theVoiceOfThePeople"/>,
+    <SimpleAreaChart key='mockGraph'/>
   ],
   document.getElementById('root')
 );
