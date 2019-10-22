3f1c67d2ef44e0fe154ab9c5783dce53db51aab4
Eamon <eamonbontempo@aol.com>
2019-10-22 16:00:39 -0400

    update outliers.js

--- src/jsLib/outliers.js
+++ src/jsLib/outliers.js
@@ -1,122 +1,145 @@
-//Finds PDF of the volume for each strike price volume pair 
-//PMF may work better since  Volumes are technically discrete 
-//Distribution functions used: https://github.com/AndreasMadsen/distributions
+//Finds outliers in volume for each strike price per expiration.
+//Compares the PDF of each strike price with to its % of total volume for the expiry.
+//An outlier's % of total volume will differ signifigantly from its PDF
 
-function getMean(vols){
+
+function getMean(dist){
 //returns mean of set 
 	var sum = 0;
 
-	for(const v of vols){
-		sum += parseInt(v);
+	for(var v of dist){
+		sum += v;
 	}
 	
-	return (sum / vols.length);
+	return (sum / dist.length);
 }
 
-function getSD(vols){
+function getSD(dist, mean){
 //returns standard devitation given set 
+	
+	var sum = 0;
 
-	var mean;
-	mean = getMean(vols);
-	var squares = [];
-	var SD;
-
-	for(const v of vols){		
-		var foo = Math.pow(parseInt(v) - mean,2);
-		squares.push(foo.toString());
+	for(var v of dist){		
+		sum += Math.pow(v - mean,2);
 	}
 
-	mean = getMean(squares);
-	SD = Math.sqrt(mean);
-
-	if(SD <= 0 ){
+	if(sum <= 0 ){
 		console.log("Error: SD <= 0");
-		return 1;
+		return 0;
 	}
 
-	return Math.sqrt(mean);
+	return Math.sqrt(sum/(dist.length));
 
 }
 
-function getOutliers(vols, std){
-//Finds outliers in a set given standard deviation
+function getPDF(x, mean, std){
+//returns PDF of a random variable given distribution info (mean and standard deviation)
 
-	var outliers = []
-	for(var i = 0; i < vols[i]; i ++){
+	var variance = std * std;
+    var m = std * Math.sqrt(2 * Math.PI);
+    var e = Math.exp(-Math.pow(x - mean, 2) / (2 * variance));
+    return e / m;
+}
 
-		if(vols[i] > (getMean(vols) + (3 * std)) || vols[i] < (getMean(vols) - (3 * std))){
-			outliers.push([i, vols[i]]);	//[option #, option volume]
+function getOutliers(strikes, vols, mean, std, dataset){
+//Finds outliers in a set by comparing the probability density function of the strike price to its % of total volume
+//Currently not in use becuase does not update status
+
+	var pdf;
+	var prob;
+	var volSum = vols.reduce((a,b) => a + b, 0);
+
+	for(var i = 0; i < strikes.length; i ++){
+		pdf = getPDF(strikes[i], mean, std);
+		prob = vols[i] / volSum;
+		strikes[i].status = "boofed";
+	}
+}
+
+function setDistribution(strikes, vols){
+//creates a distribution based on the volume for each strike price. 
+
+	var data = [];
+	for(var i = 0; i < strikes.length; i ++){
+		for(var j = 0; j < vols[i]; j ++){
+			data.push(strikes[i]);
 		}
 	}
 
-	return outliers;
-
+	return data;
 }
 
 //Main
-const fs = require('fs')
-var distributions = require('distributions');
-//var gaussian = require('gaussian');
+const fs = require('fs');
 
-fs.readFile('/Users/Eamon/Desktop/data.json', 'utf8', (err, jsonString) => {	//file path for data here
+fs.readFile('/Users/Eamon/Desktop/OutsmartOptions/data.json', 'utf8', (err, jsonString) => {	//file path for data here
     if (err) {
         console.log("File read failed:", err)
         return
     }
 
-    var data = JSON.parse(jsonString);
-    var date;
+    var dataset = JSON.parse(jsonString); 
+    var exp;
 
-    var putMap = new Map();
-    var callMap = new Map();
 
 	var putVols;
 	var callVols;
-
-	var putDist; 
-	var callDist;
-
-    var mean;
-    var SD;
-
-    var putProb;
-    var callProb;
+	var strikes;
+	var dist;
+	var std;
+	var mean;
+	var pdf;
+	var ratio;
+	var volSum;
 
 
-    for(var i = 0; i < data.length; i ++){	//traverse each date 
+    for(var exp = 0; exp < dataset.length; exp++){ //traverses through each expiration 
 
-    	date = data[i];
+    	putVols = [];
+    	callVols = [];
+    	strikes = [];
 
-    	console.log("----------------" + date[0] + "-----------------");
+   
+    	for(strike of dataset[exp][1]){	//traverses through each strike price
 
-    	for(var j = 0; j < date[1].length; j ++){ //collect Volumes 
-
-    		putMap.set(date[1][j]["strike"], date[1][j]["putVol"]);
-    		callMap.set(date[1][j]["strike"], date[1][j]["callVol"]);
-
+    		strikes.push(strike['strike']);
+    		putVols.push(strike['putVol']);
+    		callVols.push(strike['callVol']);
+    		strike.status = "normal";
     	}
-  		
-  		//Calculate Distribution for puts
-  		putVols = Array.from(putMap.values());
-    	mean = getMean(putVols);
-    	SD = getSD(putVols);
-    	putDist = distributions.Normal(mean, SD);  
 
-    	//Calculate Distribution for calls
-    	callVols = Array.from(callMap.values());
-    	mean = getMean(callVols);
-    	SD = getSD(callVols);
-    	callDist = distributions.Normal(mean, SD);
+    	//check for outliers in put volumes
+    	dist = setDistribution(strikes, putVols);
+    	mean = getMean(dist);
+    	std = getSD(dist, mean);
+		volSum = putVols.reduce((a,b) => a + b, 0);
+		
+		for(var i = 0; i < strikes.length; i ++){	//traverses each strike price and checks for outliers
+			pdf = getPDF(strikes[i], mean, std);
+			ratio = putVols[i] / volSum;
 
+			if(ratio > pdf * 2){	//THRESHOLD FOR OUTLIER
+				dataset[exp][1][i].status = "fucked";
+			}
+			console.log(dataset[exp][1][i]);
+		}
 
-    	//Print Put Probabilities 
-    	for(var[s,v] of putMap){
-    		putProb = putDist.pdf(v);
-    		console.log("Strike: " + s + " PutVol: " + v + " Probability: " + putProb);
+		//check for outliers in call volumes
+		dist = setDistribution(strikes, callVols);
+    	mean = getMean(dist);
+    	std = getSD(dist, mean);
+		volSum = callVols.reduce((a,b) => a + b, 0);
+		
+		for(var i = 0; i < strikes.length; i ++){	//traverses each strike price and checks for outliers
+			pdf = getPDF(strikes[i], mean, std);
+			ratio = putVols[i] / volSum;
 
-    	}
-    	//Need to find way to loop through call and put probabilities at same time
-    	putMap.clear();
+			if(ratio > pdf * 2){	//THRESHOLD FOR OUTLIER
+				dataset[exp][1][i].status = "fucked";
+			}
+			console.log(dataset[exp][1][i]);
+		}
+
     }
-
-})
+}
+)
