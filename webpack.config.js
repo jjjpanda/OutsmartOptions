2c7bf2cb793600b769aac4b504ef653587d1cdd0
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2019-11-01 11:46:29 -0400

    updated app webpack and app html

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/app.html',
-      chunks : ['index'],
+      chunks : ['app'],
       filename: 'app.html'
     })
   ]
