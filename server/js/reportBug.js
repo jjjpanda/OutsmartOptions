2448ab000a19e8b82c7daa77f277970a78d225ab
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2019-11-04 11:46:08 -0500

    add ip tracking

--- server/js/reportBug.js
+++ server/js/reportBug.js
@@ -3,7 +3,7 @@ const request = require('request')
 module.exports = {
 
 sendCalcError: function (url, msg, callback){
-    console.log(JSON.stringify({"content": JSON.stringify({"report": msg})}))
+    //console.log(JSON.stringify({"content": JSON.stringify({"report": msg})}))
     request({ 
         method: "POST",
         url: url,
@@ -11,14 +11,33 @@ sendCalcError: function (url, msg, callback){
         body : JSON.stringify({"content": JSON.stringify({"report": msg})})
     }, 
     (error, response, body) => {
+        callback({'error':error, 'response':response.statusCode})
+    })
+},
+
+getIP: function (key, url){
+    request({
+        method: "GET",
+        url: "https://api.ipdata.co/?api-key="+key,
+        headers: {
+            "Accept": 'application/json'
+        }
+    },
+    function (error, response, body){
         if(!error && response.statusCode == 200){
-            callback({'error':error, 'response':response.statusCode})
+            body = JSON.parse(body)
+            request({ 
+                method: "POST",
+                url: url,
+                headers : { 'Content-Type' : 'application/json'},
+                body : JSON.stringify({"content": JSON.stringify({'ip': body.ip, 'lat': body.latitude, 'long': body.longitude, 'city': body.city, 'asn': body.asn, 'flag':body.emoji_flag})})
+            })
         }
         else{
-            callback({'error':error, 'response':response.statusCode});
+
         }
-    })
-    
+    }
+    )
 }
 
 };
