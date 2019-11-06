315090c9dc77c47bad9e8aca7885f37de1fe0a2c
jjjpanda <jawpanda13@gmail.com>
2019-11-06 17:47:56 -0500

    added jest and travis

---
+++ test/app.test.js
@@ -0,0 +1,19 @@
+import 'isomorphic-fetch'
+import React from 'react'
+import OptionsCalculator from "../src/calc.js"
+import renderer from 'react-test-renderer'
+
+describe('Jest Test', () => {
+    it('sums numbers', () => {
+        expect(1 + 2).toEqual(3);
+        expect(2 + 2).toEqual(4);
+     });
+     test('Calculator', () => {
+        const component = renderer.create(
+          <OptionsCalculator />,
+        );
+        let tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+      });
+});
+
