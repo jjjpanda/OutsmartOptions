787454291f05974bc3e59c069a26bab3a47dd297
Christian Smith <christian.smith0016@gmail.com>
2019-11-07 12:35:49 -0500

    more login

--- src/login.js
+++ src/login.js
@@ -15,6 +15,10 @@ class Login extends React.Component{
                     title="Login"
                 />
                 <TextField
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth
                     hintText="Input Username"
                     floatingLabelText="Username"
                     onChange = {(event,newValue) => this.setState({username:newValue})}
@@ -22,10 +26,39 @@ class Login extends React.Component{
                 <br/>
                     <TextField
                         type="password"
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
                         hintText="Input Password"
                         floatingLabelText="Password"
                         onChange = {(event,newValue) => this.setState({password:newValue})}
                     />
+                    <FormControlLabel
+                        control={<Checkbox value="remember" color="primary" />}
+                        label="Remember me"
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                    Sign In
+                    </Button>
+                    <Grid container>
+                        <Grid item xs>
+                        <Link href="#" variant="body2">
+                        Forgot password?
+                        </Link>
+                        </Grid>
+                        <Grid item>
+                        <Link href="#" variant="body2">
+                            {"Sign Up"}
+                        </Link>
+                        </Grid>
+                    </Grid>
                    <br/>
                    <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
                 </div>
