b45259ea6591926d7eed1f71e86940e2a94fa475
Christian Smith <christian.smith0016@gmail.com>
2019-11-07 13:28:02 -0500

    antd login

--- src/login.js
+++ src/login.js
@@ -1,100 +1,46 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import { makeStyles } from '@material-ui/core/styles';
+import 'antd/dist/antd.css';
+import './index.css';
+import { 
+    Input, 
+    Tooltip, 
+    Icon 
+} from 'antd';
 
 class Login extends React.Component{
     constructor(props){
         super(props)
-        this.state = {username: '', password: ''}
     }
-
-    useStyles = makeStyles(theme => ({
-        body: {
-         backgroundColor: theme.palette.common.white,
-        },  
-        paper: {
-          marginTop: theme.spacing(8),
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        },
-        avatar: {
-          margin: theme.spacing(1),
-          backgroundColor: theme.palette.secondary.main,
-        },
-        form: {
-          width: '100%',
-          marginTop: theme.spacing(1),
-        },
-        submit: {
-          margin: theme.spacing(3, 0, 2),
-        },
-      }));      
 
     render(){
         return (
             <div>
-              <MuiThemeProvider>
-                <div>
-                <AppBar
-                    title="Login"
+                <Input
+                placeholder="Username"
+                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                suffix={
+                    <Tooltip title="Enter your username">
+                    <Icon type="info-circle" style={{ color: 'rgba(0,0,0,.45)' }} />
+                    </Tooltip>
+                }
                 />
-                <TextField
-                    variant="outlined"
-                    margin="normal"
-                    required
-                    fullWidth
-                    hintText="Input Username"
-                    floatingLabelText="Username"
-                    onChange = {(event,newValue) => this.setState({username:newValue})}
+            
+                <br />
+                <br />
+
+                <Input
+                placeholder="Password"
+                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                suffix={
+                    <Tooltip title="Enter your password">
+                    <Icon type="info-circle" style={{ color: 'rgba(0,0,0,.45)' }} />
+                    </Tooltip>
+                }
                 />
-                <br/>
-                    <TextField
-                        type="password"
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        hintText="Input Password"
-                        floatingLabelText="Password"
-                        onChange = {(event,newValue) => this.setState({password:newValue})}
-                    />
-                    <FormControlLabel
-                        control={<Checkbox value="remember" color="primary" />}
-                        label="Remember me"
-                    />
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        className={classes.submit}
-                    >
-                    Sign In
-                    </Button>
-                    <Grid container>
-                        <Grid item xs>
-                        <Link href="#" variant="body2">
-                        Forgot password?
-                        </Link>
-                        </Grid>
-                        <Grid item>
-                        <Link href="#" variant="body2">
-                            {"Sign Up"}
-                        </Link>
-                        </Grid>
-                    </Grid>
-                   <br/>
-                   <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
-                </div>
-                </MuiThemeProvider>
-            </div>
-        );
+            
+            </div>,
+            mountNode
+        )
     }
 }
 
