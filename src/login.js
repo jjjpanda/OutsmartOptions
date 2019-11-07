530a814a60749d1b8fdc46e03f05d9eb07678102
Christian Smith <christian.smith0016@gmail.com>
2019-11-07 12:29:32 -0500

    Base login page

--- src/login.js
+++ src/login.js
@@ -3,12 +3,35 @@ import React from 'react';
 class Login extends React.Component{
     constructor(props){
         super(props)
+        this.state = {username: '', password: ''}
     }
 
     render(){
-        return(<div>
-            Login
-        </div>)
+        return (
+            <div>
+              <MuiThemeProvider>
+                <div>
+                <AppBar
+                    title="Login"
+                />
+                <TextField
+                    hintText="Input Username"
+                    floatingLabelText="Username"
+                    onChange = {(event,newValue) => this.setState({username:newValue})}
+                />
+                <br/>
+                    <TextField
+                        type="password"
+                        hintText="Input Password"
+                        floatingLabelText="Password"
+                        onChange = {(event,newValue) => this.setState({password:newValue})}
+                    />
+                   <br/>
+                   <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
+                </div>
+                </MuiThemeProvider>
+            </div>
+        );
     }
 }
 
