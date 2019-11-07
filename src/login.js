8d6520e28d880672f6998a1ee137f05911406409
Christian Smith <christian.smith0016@gmail.com>
2019-11-07 12:49:23 -0500

    added dependencies for login page

--- src/login.js
+++ src/login.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
 
 class Login extends React.Component{
     constructor(props){
         super(props)
         this.state = {username: '', password: ''}
     }
+
+    useStyles = makeStyles(theme => ({
+        body: {
+         backgroundColor: theme.palette.common.white,
+        },  
+        paper: {
+          marginTop: theme.spacing(8),
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        },
+        avatar: {
+          margin: theme.spacing(1),
+          backgroundColor: theme.palette.secondary.main,
+        },
+        form: {
+          width: '100%',
+          marginTop: theme.spacing(1),
+        },
+        submit: {
+          margin: theme.spacing(3, 0, 2),
+        },
+      }));      
 
     render(){
         return (
