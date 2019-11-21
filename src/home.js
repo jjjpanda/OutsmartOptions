e5d78c244f82bba9a407aae2ae137ff15e881b27
hmigdon <hmigdon@stevens.edu>
2019-11-21 15:23:39 -0500

    Update home.js
    twitter in home page

--- src/home.js
+++ src/home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { TwitterTimelineEmbed, TwitterShareButton, TwitterFollowButton, TwitterHashtagButton, TwitterMentionButton, TwitterTweetEmbed, TwitterMomentShare, TwitterDMButton, TwitterVideoEmbed, TwitterOnAirButton } from 'react-twitter-embed';
 class HomePage extends React.Component{
     constructor(props){
         super(props)
@@ -8,7 +8,12 @@ class HomePage extends React.Component{
     render(){
         return (
         <div style={{paddingLeft:'60px', paddingTop:'20px'}}>
-            <h1 key = "mainTitle" >Outsmart Options</h1>
+             <TwitterTimelineEmbed
+                sourceType="profile"
+                screenName="Outsmart Options"
+                options={{height: 400}}
+                />
+                <a class="twitter-timeline" href="https://twitter.com/OutsmartO">Outsmart Options on Twitter</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
             <br />
             <div> 
                 What up Youtube. 
