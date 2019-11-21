26e4e594e92f6cae131019e850e1cc5dcf5d1e76
Eamon <eamonbontempo@aol.com>
2019-11-21 15:33:20 -0500

    create about.js

---
+++ src/about.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Help extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return()
+ 
+    }
+}
+
+export default Help;
