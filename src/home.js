785557641c547e5263c5131a2db7e7700760b80e
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2019-11-20 20:23:49 -0500

    home page stock symbol

--- src/home.js
+++ src/home.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import {StockSymbol} from './calc.js'
 
 class HomePage extends React.Component{
     constructor(props){
         super(props)
+        this.state = {
+            price: 0,
+            priceChange: 0,
+            exists: true
+        }
     }
 
     render(){
         return (
         <div style={{paddingLeft:'60px', paddingTop:'20px'}}>
             <h1 key = "mainTitle" >Outsmart Options</h1>
+
+            <StockSymbol onSearch={this.onSearch} price={this.state.price} priceChange={this.state.priceChange} exists={this.state.exists}/>
+      
             <br />
             <div> 
                 What up Youtube. 
@@ -22,4 +31,4 @@ class HomePage extends React.Component{
     )}
 }
 
-export default HomePage
+export {HomePage}
