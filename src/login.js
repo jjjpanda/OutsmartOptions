0b38a8b865a6dcd8d39687fe7192eb96fa442f13
Christian Smith <christian.smith0016@gmail.com>
2019-11-26 15:10:18 -0500

    Update login.js

--- src/login.js
+++ src/login.js
@@ -3,7 +3,6 @@ import {
     Input, 
     Tooltip, 
     Icon,
-    Form,
     Button
 } from 'antd';
 
@@ -12,54 +11,26 @@ class Login extends React.Component{
         super(props)
     }
 
-    componentDidMount() {
-        this.props.form.validateFields();
-      }
-    
-    handleSubmit = e => {
-        e.preventDefault();
-        this.props.form.validateFields((err, values) => {
-            if (!err) {
-                console.log('Received values of form: ', values);
-            }
-        });
-    };
-
     render() {
-        const { getFieldDecorator, getFieldsError, getFieldError, isFieldTouched } = this.props.form;
-    
-        const usernameError = isFieldTouched('username') && getFieldError('username');
-        const passwordError = isFieldTouched('password') && getFieldError('password');
         return (
-            <Form layout="inline" onSubmit={this.handleSubmit}>
-            <Form.Item validateStatus={usernameError ? 'error' : ''} help={usernameError || ''}>
-                {getFieldDecorator('username', {
-                    rules: [{ required: true, message: 'Please input your username' }],
-                })(
-                <Input
-                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                    placeholder="Username"
-                />,
-                )}
-            </Form.Item>
-            <Form.Item validateStatus={passwordError ? 'error' : ''} help={passwordError || ''}>
-                {getFieldDecorator('password', {
-                    rules: [{ required: true, message: 'Please input your password' }],
-                })(
-                <Input
-                    prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                    type="password"
-                    placeholder="Password"
-                />,
-                )}
-            </Form.Item>
-            <Form.Item>
-                <Button type="primary" htmlType="submit" disabled={hasErrors(getFieldsError())}>
-                    Log in
-                </Button>
-            </Form.Item>
-            </Form>
-        ); 
+            <div>
+            <Input
+                placeholder="Enter Username"
+                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                suffix={
+                <Tooltip title="Extra information">
+                    <Icon type="info-circle" style={{ color: 'rgba(0,0,0,.45)' }} />
+                </Tooltip>
+                }
+            />
+
+            <br />
+            <br />
+
+            <Input.Password placeholder="Enter Password" />
+            </div>,
+            document.getElementById('container')
+        )
     }
 }
 
