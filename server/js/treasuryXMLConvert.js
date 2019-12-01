9895f1058dac017de3a5ee0143bebf006667fb63
jjjpanda <jawpanda13@gmail.com>
2019-12-01 11:02:58 -0500

    the US treasury is a fickle beast

--- server/js/treasuryXMLConvert.js
+++ server/js/treasuryXMLConvert.js
@@ -4,15 +4,30 @@ const convert = require('xml-js');
 module.exports = {
 
 getYield: function (callback){
+    var date = new Date()
+    if(date.getDay() == 0){
+        date.setDate(date.getDate()-2)
+    }
+    else if(date.getDay() == 6){
+        date.setDate(date.getDate()-1)
+    }
+    var year = date.getFullYear()
+    var month = date.getMonth()+1
+    var day  = date.getDate()
     request({
         method: 'get',
-        url: 'https://data.treasury.gov/feed.svc/DailyTreasuryYieldCurveRateData?$filter=month(NEW_DATE)%20eq%20'+((new Date()).getMonth()+1)+'%20and%20year(NEW_DATE)%20eq%20'+((new Date()).getFullYear()),
+        url: 'https://data.treasury.gov/feed.svc/DailyTreasuryYieldCurveRateData?$filter=month(NEW_DATE)%20eq%20'+month+'%20and%20year(NEW_DATE)%20eq%20'+year+'and%20day(NEW_DATE)%20eq%20'+day,
         }, (error, response, body) => {
         if(!error && response.statusCode == 200){
             //console.log(body)
             data = (convert.xml2json(body))
-            
-            data = JSON.parse(data).elements[0].elements[4].elements[6].elements[0].elements.slice(2, 14).map(k => ({name: k.name, val: parseFloat(k.elements[0].text)}))
+            try {
+                data = JSON.parse(data).elements[0].elements[4].elements[6].elements[0].elements.slice(2, 14).map(k => ({name: k.name, val: parseFloat(k.elements[0].text)}))
+            }
+            catch(error) {
+                //console.error(error);
+                data = []
+            }
             callback(data)
         }
         else{
