9b59b1a346b6d82479e6a5a5890d06db8a574e18
jjjpanda <jawpanda13@gmail.com>
2019-12-08 10:46:05 -0500

    models for the server

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-// Create Schema
+
 const UserSchema = new Schema({
   name: {
     type: String,
@@ -19,4 +19,4 @@ const UserSchema = new Schema({
     default: Date.now
   }
 });
-module.exports = User = mongoose.model("users", UserSchema);
+module.exports = mongoose.model("users", UserSchema);
