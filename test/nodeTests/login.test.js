ed5a171d57f352461afdc61373b58c7117afabeb
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2019-12-16 23:31:04 -0500

    adding supertests 🦸‍♂️ for api testing

---
+++ test/nodeTests/login.test.js
@@ -0,0 +1,22 @@
+const app = require('../../server/app.js')
+const request = require('supertest');
+const mongoDB = require('../../server/database')
+
+describe('Login Tests', () => {
+
+    beforeAll(async (done) => {
+        mongoDB.connect(done);
+    }, 50000);
+    
+    afterAll(async (done) => {
+        mongoDB.disconnect(done);
+    }, 50000);
+
+    it('Gets /', async (done) => {
+        request(app).get("/").then(response => {
+            expect(response.statusCode).toBe(200)
+            done()
+        })
+    }, 10000)
+
+})
--- test/reactTests/calc.test.js
+++ test/reactTests/calc.test.js
@@ -1,8 +1,8 @@
 import 'isomorphic-fetch';
+
 import React from 'react';
 import { shallow, configure } from 'enzyme';
 import renderer from 'react-test-renderer';
-
 
 import Adapter from 'enzyme-adapter-react-16';
 import OptionsCalculator from '../../src/calc.js';
