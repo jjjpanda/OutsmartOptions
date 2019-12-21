d8045440c21b20525bfb50157bacfb1e87a9d2da
jjjpanda <jawpanda13@gmail.com>
2019-12-21 14:23:35 -0500

    jest coverage collection

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,8 @@
 module.exports = (api) => {
     api.cache(true);
     const presets = [
-        "@babel/preset-env","@babel/preset-react"
+        "@babel/preset-env",
+        "@babel/preset-react"
     ]
     const plugins =  [
         ["import", {"libraryName": "antd", "style": true} ],
