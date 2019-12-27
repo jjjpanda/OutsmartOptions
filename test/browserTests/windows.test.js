ffb506b2ece7396b160823e0ad5c311f82cc2a80
jjjpanda <jawpanda13@gmail.com>
2019-12-26 19:07:41 -0500

    ❌cross🌐browser🧪testing

--- test/browserTests/windows.test.js
+++ test/browserTests/windows.test.js
@@ -1,11 +1,10 @@
 import * as eleManip from './elementManipulation.js';
-import * as capabilities from './windowsBrowsers.js';
+import * as capabilities from './browsers.js';
 
 const webdriver = require('selenium-webdriver');
 const browserstack = require('browserstack-local');
 const env = require('dotenv').config();
 
-let driver;
 const bs_local = new browserstack.Local();
 const bs_local_args = { key: capabilities.credentials['browserstack.key'], onlyAutomate: true, logFile: 'test/browserTests/log.txt' };
 
@@ -37,8 +36,10 @@ afterAll(async () => {
   await stop();
 }, 10000);
 
-for (var capability of capabilities.capabilities) {
+for (let capability of capabilities.capabilities) {
   describe(capability.name, () => {
+    let driver;
+
     beforeAll(async (done) => {
       driver = new webdriver.Builder()
         .usingServer(`http://${capabilities.credentials['browserstack.user']}:${capabilities.credentials['browserstack.key']}@hub-cloud.browserstack.com/wd/hub`)
@@ -47,18 +48,56 @@ for (var capability of capabilities.capabilities) {
 
       await driver.get('https://outsmart.herokuapp.com').then(() => {
         done();
+      }, () => {
+        done.fail(new Error("Website Didn't Load."))
       });
     }, 50000);
 
     afterAll(async (done) => {
-      await driver.quit();
-      done();
+      await driver.quit().then(() => {
+        done();
+      }, () => {
+        done.fail(new Error("Issue with disconnecting."))
+      });
     }, 10000);
 
-    it(capability.name, async (done) => {
+    it('React Root Exists', async (done) => {
       const root = await eleManip.getElementById(webdriver, driver, 'root');
       expect(root).toBeDefined();
       done();
     }, 10000);
+
+    it('Go to Calculator', async (done) => {
+      await driver.get('https://outsmart.herokuapp.com/calc').then(() => {
+        done();
+      }, () => {
+        done.fail(new Error("Website Didn't Load."))
+      });
+    }, 10000)
+
+    it('Go to Watchlist', async (done) => {
+      await driver.get('https://outsmart.herokuapp.com/watch').then(() => {
+        done();
+      }, () => {
+        done.fail(new Error("Website Didn't Load."))
+      });
+    }, 10000)
+
+    it('Go to HelpPage', async (done) => {
+      await driver.get('https://outsmart.herokuapp.com/help').then(() => {
+        done();
+      }, () => {
+        done.fail(new Error("Website Didn't Load."))
+      });
+    }, 10000)
+
+    it('Go to AboutPage', async (done) => {
+      await driver.get('https://outsmart.herokuapp.com/about').then(() => {
+        done();
+      }, () => {
+        done.fail(new Error("Website Didn't Load."))
+      });
+    }, 10000)
+    
   });
 }
