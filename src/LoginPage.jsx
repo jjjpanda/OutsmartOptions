a0411caab1a4eda74a029b15fe01d1f9bb96e72e
Christian Smith <christian.smith0016@gmail.com>
2020-01-23 14:54:38 -0500

    Login updates

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -1,35 +1,64 @@
 import React from 'react';
 import {
   Input,
-  Tooltip,
-  Icon,
+  Form,
+  Checkbox,
   Button,
 } from 'antd';
+import { 
+  UserOutlined,
+  LockOutlined, 
+} from '@ant-design/icons';
 
 class LoginPage extends React.Component {
   constructor(props) {
     super(props);
+    onFinish = values => {
+      console.log('Received values of form: ', values);
+    };
   }
 
   render() {
     return (
-      <div>
-        <Input
-          placeholder="Enter Username"
-          prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-          suffix={(
-            <Tooltip title="Extra information">
-              <Icon type="info-circle" style={{ color: 'rgba(0,0,0,.45)' }} />
-            </Tooltip>
-              )}
-        />
+      <Form
+        name="login"
+        className="login-form"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+      >
+      <Form.Item
+        name="username"
+        rules={[{ required: true, message: 'Enter your username' }]}
+      >
+        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        rules={[{ required: true, message: 'Enter your password' }]}
+      >
+      <Input
+        prefix={<LockOutlined className="site-form-item-icon" />}
+        type="password"
+        placeholder="Password"
+      />
+      </Form.Item>
+      <Form.Item>
+        <Form.Item name="remember" valuePropName="checked" noStyle>
+          <Checkbox>Remember Me</Checkbox>
+        </Form.Item>
 
-        <br />
-        <br />
+        <a className="login-form-forgot" href="">
+          Forgot Password
+        </a>
+      </Form.Item>
 
-        <Input.Password placeholder="Enter Password" />
-      </div>,
-      document.getElementById('container')
+      <Form.Item>
+        <Button type="primary" htmlType="submit" className="login-form-button">
+          Log In
+        </Button>
+        Or <a href="">Register</a>
+      </Form.Item>
+    </Form>
     );
   }
 }
