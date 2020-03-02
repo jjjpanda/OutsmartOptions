682ceb6174d8983acd04b1a0b7f48cf9c21b535d
jjjpanda <jawpanda13@gmail.com>
2020-03-02 12:17:25 -0500

    setting up IV calendar 🛹

---
+++ src/components/StockCalendar.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import {
+  Calendar,
+  Badge,
+  Icon
+} from 'antd';
+
+import * as moment from 'moment';
+
+class StockCalendar extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    dateCellRender = (value) => {
+        if(moment(this.props.earningsDate).diff(value, 'days') < 1){
+            return (
+                <h1>{value.date()}</h1>
+            )
+        }
+        else{
+            return (
+                <strong>{value.date()}</strong>
+            )
+        }
+    }
+    
+    onDateChange = (value, m) => {
+        console.log(value)
+
+    }
+
+    render() {
+        return (
+            <Calendar fullscreen ={false} onSelect = {this.onDateChange} dateFullCellRender = {this.dateCellRender}/>
+        )
+    }
+}
+
+export default StockCalendar;
