ad90ae01d6ac1e1bc548b8eddbd5202942c1f141
jjjpanda <jawpanda13@gmail.com>
2020-03-21 01:48:43 -0400

    updates 😂😂😂😂😂😂😂😂😂😂😂😂😂

--- server/routes/calculation/optionsQuote.js
+++ server/routes/calculation/optionsQuote.js
@@ -1,8 +1,16 @@
 module.exports = {
 
     getOptionsQuote(req, res, next) {
-        for(const expiry of req.body.answer.chain){
-            
+        req.body.answer.optionsQuote = {callOI:0, putOI:0, callVol:0, putVol:0}
+        for(const [date, strikes] of req.body.answer.chain){
+            for(const option of strikes){
+                req.body.answer.optionsQuote.callOI += option.callOI
+                req.body.answer.optionsQuote.putOI += option.putOI
+                
+                req.body.answer.optionsQuote.callVol += option.callVol
+                req.body.answer.optionsQuote.putVol += option.putVol
+
+            }
         }
         next()
     }
