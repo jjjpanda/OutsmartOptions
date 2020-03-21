2801ffc06d3b3dfefdbb7d0c320f1bed05975900
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2020-03-20 22:46:42 -0400

    updates to the server 😍😍😍 pt idek

--- server/routes/buffer/treasuryBuffer.js
+++ server/routes/buffer/treasuryBuffer.js
@@ -8,34 +8,45 @@ module.exports = {
 
   getYieldCurve(req, res, next) {
     const d = moment();
-    while(!d.isoWeekday()){
-      d.subtract(1, 'days');
-    }
-    const year = d.year()
-    const month = d.month()+1
-    const date = d.date()
-    request({
-      method: 'get',
-      url: `https://data.treasury.gov/feed.svc/DailyTreasuryYieldCurveRateData?$filter=month(NEW_DATE)%20eq%20${month}%20and%20year(NEW_DATE)%20eq%20${year}and%20day(NEW_DATE)%20eq%20${date}`,
-    }, (error, response, body) => {
-      if (!error && response.statusCode == 200) {
-        let data = (convert.xml2json(body));
-        try {
-          data = JSON.parse(data).elements[0].elements[4].elements[6].elements[0].elements.slice(2, 14).map((k) => ({ name: k.name, val: parseFloat(k.elements[0].text) }));
-        } catch (error) {
-          //appendLogs('./server/logs/logs.txt', error);
-          data = [];
+    let i = 5; //5 tries max
+    looper = () => {
+      while(!d.isoWeekday()){
+        d.subtract(1, 'days');
+      }
+      const year = d.year()
+      const month = d.month()+1
+      const date = d.date()
+      request({
+        method: 'get',
+        url: `https://data.treasury.gov/feed.svc/DailyTreasuryYieldCurveRateData?$filter=month(NEW_DATE)%20eq%20${month}%20and%20year(NEW_DATE)%20eq%20${year}and%20day(NEW_DATE)%20eq%20${date}`,
+      }, (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+          let data = (convert.xml2json(body));
+          try {
+            data = JSON.parse(data).elements[0].elements[4].elements[6].elements[0].elements.slice(2, 14).map((k) => ({ name: k.name, val: parseFloat(k.elements[0].text) }));
+          } catch (error) {
+            //appendLogs('./server/logs/logs.txt', error);
+            data = [];
+          }
+          if(i > 0 && data.length==0){
+            d.subtract(1, 'days');
+            i--;
+            looper()
+          }
+          else{
+            req.body.answer.yields = data
+            next()
+          }
         }
-        req.body.answer = data
-        next()
-      }
-      else{
-        res.json({ error: true, details: "Data Formatting Error" })
-      }
-    })
+        else{
+          res.json({ error: true, details: "Data Formatting Error" })
+        }
+      })
+    }
+    looper()
   },
 
-  getYield(callback) {
+  getYieldDepr(callback) {
     const date = new Date();
     if (date.getDay() == 0) {
       date.setDate(date.getDate() - 2);
--- server/routes/market.js
+++ server/routes/market.js
@@ -10,23 +10,22 @@ const alphakey = process.env.alpha;
 //const Earnings = require('../daemons/db/Earnings');
 
 const validate = require('./validation/marketDataRequestValidation.js')
+const prepare = require('./validation/prepareAnswer.js')
 
 const tradierBuffer = require('./buffer/tradierBuffer.js')
+const yFinanceBuffer = require('./buffer/yFinanceBuffer.js')
 const treasuryBuffer = require('./buffer/treasuryBuffer.js')
 
+const optionsQuote = require('./calculation/optionsQuote.js')
 const noCheckSend = require('./calculation/noCheckSend.js')
 
 const realTimeData = require('./buffer/realTimeData.js');
 
-router.post('/price', validate.validateTicker, tradierBuffer.getQuotes, noCheckSend);
+router.post('/price', validate.validateTicker, prepare, tradierBuffer.getQuotes, yFinanceBuffer.getQuote, noCheckSend("quote"));
 
-router.post('/chain', validate.validateTicker, tradierBuffer.getChainExpiries, noCheckSend);
+router.post('/chain', validate.validateTicker, prepare, tradierBuffer.getQuotes, yFinanceBuffer.getQuote, treasuryBuffer.getYieldCurve, tradierBuffer.getChainExpiries, noCheckSend("chain"));
 
-router.post('/optionsQuote', validate.validateTicker, (req, res) => {
-  realTimeData.getOptionsQuote(tradikey, req.body.ticker, (data) => {
-    res.json(data);
-  });
-})
+router.post('/optionsQuote', validate.validateTicker, prepare, tradierBuffer.getQuotes, yFinanceBuffer.getQuote, treasuryBuffer.getYieldCurve, tradierBuffer.getChainExpiries, optionsQuote.getOptionsQuote, noCheckSend("optionsQuote"));
 
 router.post('/iv', validate.validateTicker, validate.validateIVLength, (req, res) => {
   realTimeData.getIV(tradikey, req.body.ticker, req.body.length, (data) => {
@@ -47,19 +46,7 @@ router.post('/guessSymbol', validate.validateText, (req, res) => {
   });
 });
 
-router.post('/divYield', validate.validateTicker, (req, res) => {
-  realTimeData.getDividend(alphakey, req.body.ticker, (data) => {
-    res.json(data);
-  });
-});
-
-router.post('/earningsDate', validate.validateTicker, (req, res) => {
-  realTimeData.getEarnings(req.body.ticker, (data) => {
-    res.json(data);
-  });
-});
-
-router.post('/treasury', treasuryBuffer.getYieldCurve, noCheckSend);
+router.post('/yields', prepare, treasuryBuffer.getYieldCurve, noCheckSend("yields"));
 
 /* 
 router.post('/earningsSoon', (req, res) => {
@@ -70,6 +57,18 @@ router.post('/earningsSoon', (req, res) => {
     } else {
       res.json({ erSoon: false });
     }
+  });
+});
+
+router.post('/divYield', validate.validateTicker, (req, res) => {
+  realTimeData.getDividend(alphakey, req.body.ticker, (data) => {
+    res.json(data);
+  });
+});
+
+router.post('/earningsDate', validate.validateTicker, (req, res) => {
+  realTimeData.getEarnings(req.body.ticker, (data) => {
+    res.json(data);
   });
 });
 */
