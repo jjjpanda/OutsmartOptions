8542df87099e430a68fb50bd898565f8a2682989
jjjpanda <jawpanda13@gmail.com>
2020-03-21 14:44:00 -0400

    updates

---
+++ server/routes/buffer/userBuffer.js
@@ -0,0 +1,152 @@
+const env = require('dotenv').config();
+
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const validate = require("../validation/loginValidation.js");
+const secretOrKey = process.env.SECRETKEY;
+
+const User = require('../../daemons/models/User');
+
+const appendLogs = require('../../logs/appendLogs.js');
+
+module.exports = {
+
+    registerUser(req, res) {
+        // Form validation
+        const { errors, isValid } = validate.validateRegisterInput(req.body);
+        // Check validation
+        if (!isValid) {
+            return res.status(400).json(errors);
+        }
+        User.findOne({ email: req.body.email }).then((user) => {
+            if (user) {
+            return res.status(400).json({ email: 'Email already exists' });
+            }
+            const newUser = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            });
+            // Hash password before saving in database
+            bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+                if (err) throw err;
+                newUser.password = hash;
+                newUser
+                .save()
+                .then((user) => res.json(user))
+                .catch((err) => appendLogs('./server/logs/logs.txt', err));
+            });
+            });
+        });
+    },
+
+    loginUser(req, res) {
+        // Form validation
+        const { errors, isValid } = validate.validateLoginInput(req.body);
+        // Check validation
+        if (!isValid) {
+          return res.status(400).json(errors);
+        }
+        const { email } = req.body;
+        const { password } = req.body;
+        // Find user by email
+        User.findOne({ email }).then((user) => {
+          // Check if user exists
+          if (!user) {
+            return res.status(404).json({ emailnotfound: 'Email not found' });
+          }
+          // Check password
+          bcrypt.compare(password, user.password).then((isMatch) => {
+            if (isMatch) {
+              // User matched
+              // Create JWT Payload
+              const payload = {
+                id: user.id,
+                name: user.name,
+              };
+              // Sign token
+              jwt.sign(
+                payload,
+                secretOrKey + user.id,
+                {
+                  expiresIn: 31556926, // 1 year in seconds
+                },
+                (err, token) => {
+                  res.json({
+                    success: true,
+                    id: user.id,
+                    token: `Bearer ${token}`,
+                  });
+                },
+              );
+            } else {
+              return res
+                .status(400)
+                .json({ passwordincorrect: 'Password incorrect' });
+            }
+          });
+        });
+    },
+
+    currentUser(req, res) {
+        const { id } = req.body;
+        User.findById(id).then((user) => {
+          if (!user) {
+            return res.sendStatus(400);
+          }
+      
+          return res.json({ user: user.name, email: user.email });
+        }).catch((error) => {
+          res.json({ error });
+        });
+    },
+
+    changePassword(req, res) {
+        const {
+          id, oldPassword, newPassword, newPassword2,
+        } = req.body;
+        if (oldPassword == undefined || newPassword == undefined || newPassword2 == undefined) {
+          return res.sendStatus(400);
+        }
+        User.findById(id).then((user) => {
+          if (!user) {
+            return res.sendStatus(400);
+          }
+      
+          bcrypt.compare(oldPassword, user.password).then((isMatch) => {
+            if (isMatch) {
+              if (newPassword == newPassword2) {
+                bcrypt.genSalt(10, (err, salt) => {
+                  bcrypt.hash(newPassword, salt, (err, hash) => {
+                    if (err) throw err;
+                    user.password = hash;
+                    user
+                      .save()
+                      .then((user) => res.json({ changed: true }))
+                      .catch((err) => res.json({ changed: false }));
+                  });
+                });
+              } else {
+                res.json({ error: 'Passwords don\'t match' });
+              }
+            } else {
+              res.json({ error: 'Incorrect Password' });
+            }
+          });
+        });
+    },
+
+    deleteUser(req, res, next) {
+        const { id } = req.body;
+        User.findByIdAndDelete(id).then((err, user) => {
+          if (!err) {
+            return res.sendStatus(400);
+          }
+      
+          return res.json({ deleted: true });
+        });
+    }
+
+}
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,154 +1,19 @@
 const express = require('express');
 
 const router = express.Router();
-const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const auth = require('./validation/authorizeUser')(jwt);
-const env = require('dotenv').config();
 
-const secretOrKey = process.env.SECRETKEY;
+const userBuffer = require("./buffer/userBuffer.js")
 
-const validate = require("./validation/loginValidation.js");
+router.post('/register', userBuffer.registerUser);
 
-const appendLogs = require('../logs/appendLogs.js');
+router.post('/login', userBuffer.loginUser);
 
-const User = require('../daemons/models/User');
+router.post('/current', auth, userBuffer.currentUser);
 
-router.post('/register', (req, res) => {
-  // Form validation
-  const { errors, isValid } = validate.validateRegisterInput(req.body);
-  // Check validation
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
-  User.findOne({ email: req.body.email }).then((user) => {
-    if (user) {
-      return res.status(400).json({ email: 'Email already exists' });
-    }
-    const newUser = new User({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    });
-    // Hash password before saving in database
-    bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(newUser.password, salt, (err, hash) => {
-        if (err) throw err;
-        newUser.password = hash;
-        newUser
-          .save()
-          .then((user) => res.json(user))
-          .catch((err) => appendLogs('./server/logs/logs.txt', err));
-      });
-    });
-  });
-});
+router.post('/change', auth, userBuffer.changePassword);
 
-router.post('/login', (req, res) => {
-  // Form validation
-  const { errors, isValid } = validate.validateLoginInput(req.body);
-  // Check validation
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
-  const { email } = req.body;
-  const { password } = req.body;
-  // Find user by email
-  User.findOne({ email }).then((user) => {
-    // Check if user exists
-    if (!user) {
-      return res.status(404).json({ emailnotfound: 'Email not found' });
-    }
-    // Check password
-    bcrypt.compare(password, user.password).then((isMatch) => {
-      if (isMatch) {
-        // User matched
-        // Create JWT Payload
-        const payload = {
-          id: user.id,
-          name: user.name,
-        };
-        // Sign token
-        jwt.sign(
-          payload,
-          secretOrKey + user.id,
-          {
-            expiresIn: 31556926, // 1 year in seconds
-          },
-          (err, token) => {
-            res.json({
-              success: true,
-              id: user.id,
-              token: `Bearer ${token}`,
-            });
-          },
-        );
-      } else {
-        return res
-          .status(400)
-          .json({ passwordincorrect: 'Password incorrect' });
-      }
-    });
-  });
-});
-
-router.post('/current', auth, (req, res) => {
-  const { id } = req.body;
-  User.findById(id).then((user) => {
-    if (!user) {
-      return res.sendStatus(400);
-    }
-
-    return res.json({ user: user.name, email: user.email });
-  }).catch((error) => {
-    res.json({ error });
-  });
-});
-
-router.post('/change', auth, (req, res) => {
-  const {
-    id, oldPassword, newPassword, newPassword2,
-  } = req.body;
-  if (oldPassword == undefined || newPassword == undefined || newPassword2 == undefined) {
-    return res.sendStatus(400);
-  }
-  User.findById(id).then((user) => {
-    if (!user) {
-      return res.sendStatus(400);
-    }
-
-    bcrypt.compare(oldPassword, user.password).then((isMatch) => {
-      if (isMatch) {
-        if (newPassword == newPassword2) {
-          bcrypt.genSalt(10, (err, salt) => {
-            bcrypt.hash(newPassword, salt, (err, hash) => {
-              if (err) throw err;
-              user.password = hash;
-              user
-                .save()
-                .then((user) => res.json({ changed: true }))
-                .catch((err) => res.json({ changed: false }));
-            });
-          });
-        } else {
-          res.json({ error: 'Passwords don\'t match' });
-        }
-      } else {
-        res.json({ error: 'Incorrect Password' });
-      }
-    });
-  });
-});
-
-router.post('/delete', auth, (req, res, next) => {
-  const { id } = req.body;
-  User.findByIdAndDelete(id).then((err, user) => {
-    if (!err) {
-      return res.sendStatus(400);
-    }
-
-    return res.json({ deleted: true });
-  });
-});
+router.post('/delete', auth, userBuffer.deleteUser);
 
 module.exports = router;
