48f463c4a395a9228d30ef93cea48334609a242c
jjjpanda <jawpanda13@gmail.com>
2020-03-31 17:49:40 -0400

    taking down god ⚔ class OptionsCalc 💥

--- que-series/lib/utilique/structuresEditingLibrary.js
+++ que-series/lib/utilique/structuresEditingLibrary.js
@@ -37,3 +37,41 @@ export function dataURItoBlob(dataURI) {
   const blob = new Blob([ab], { type: mimeString });
   return blob;
 }
+
+export function dataToGraphConversion(data) {
+  const dataConverted = [];
+
+  const keyNameObj = { x: 0 };
+  for (var option of data) {
+    for (var i = data[0].profit.length - 1, factor = Math.round(data[0].profit.length / 7) + 1; i >= 0; i -= factor) {
+      keyNameObj[`${option.key}a${option.profit[i][0]}`] = 0;
+    }
+  }
+  for (var j = 0; j < data[0].profit[0][1].length; j++) {
+    // console.log(option.key+ option.profit[i][0]+ option.profit[i][1][j])
+    dataConverted.push({ ...keyNameObj });
+    dataConverted[dataConverted.length - 1].x = data[0].profit[0][1][j][0];
+  }
+  for (var option of data) {
+    for (var i = data[0].profit.length - 1, factor = Math.round(data[0].profit.length / 7) + 1; i >= 0; i -= factor) {
+      for (var j = 0; j < data[0].profit[i][1].length; j++) {
+        dataConverted[j][`${option.key}a${option.profit[i][0]}`] = option.profit[i][1][j][1];
+      }
+    }
+  }
+  return dataConverted;
+}
+
+export function dataToTableConversion(data) {
+  const dataConverted = [];
+
+  for (let i = data[0][1].length - 1, end = 0; i >= end; i--) {
+    const o = {};
+    o.x = data[0][1][i][0].toFixed(2);
+    for (const date of data) {
+      o[date[0]] = date[1][i][1].toFixed(2);
+    }
+    dataConverted.push(o);
+  }
+  return dataConverted;
+}
