d190782095b263b8dd99f0940453db353a4f19bd
jjjpanda <jawpanda13@gmail.com>
2020-04-04 22:55:30 -0400

    more god-slaying ⚔ epic modular 🧩 OptCalc edits

---
+++ src/components/ReportModal.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import {
+  Input,
+  InputNumber,
+  Button,
+  Switch,
+  Modal,
+  Table,
+} from 'antd';
+
+import SpinningLogo from './SpinningLogo.jsx';
+
+import {
+    aesthetique as aes,
+    mathematique as math,
+    utilique as util,
+} from 'que-series';
+const { request, structure } = util;
+
+import html2canvas from 'html2canvas';
+
+class ReportModal extends React.Component {
+
+    constructor(props){
+        super(props)
+        this.state= {
+            reportLoading: false
+        }
+    }
+
+    sendCalcError = () => {
+        this.setState(() => ({ reportLoading: true }), () => {
+          const input = document.getElementsByTagName('html')[0];
+          html2canvas(input).then((c) => {
+            const base64image = c.toDataURL('image/png');
+            const formData = new FormData();
+            formData.append('file', structure.dataURItoBlob(base64image), 'img');
+            request.fileReq('/api/bug/imageReport', formData);
+          });
+          request.postFetchReq('/api/bug/report',
+            JSON.stringify({
+              options: this.props.optionsSelected
+                .map((option) => Object.keys(option).filter((key) => key != 'profit')
+                  .reduce((obj, key) => {
+                    obj[key] = option[key];
+                    return obj;
+                  },
+                  {})),
+            }),
+            (data) => {
+              this.setState(() => ({ reportLoading: false }));
+            });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <Button onClick={this.sendCalcError} loading={this.state.reportLoading}>Report Calculation Error</Button>
+                <Modal
+                    visible={this.state.reportLoading}
+                    closable={false}
+                    maskClosable={false}
+                    footer = {null}
+                >
+                    <SpinningLogo />
+                </Modal>
+            </div>
+        )
+    }
+}
+
+export default ReportModal;
